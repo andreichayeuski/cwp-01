88a7be9fbe8bbbe3a977dbf81903a9a08dd84d2d
Andrei Chayeuski <andreichayeuski@gmail.com>
2018-09-16 22:39:59 +0300

    validation of the directory's path, make file

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 
-/* first task
+/* task02
 console.log('Hello World');*/
 
-/* second task
-const name = process.argv[2];
+/* task03
+const name= process.argv[2];
 console.log(`Hi ${name}!`);*/
 
-/* third task */
-process.argv.forEach(function(element) {
-    console.log(element);
-  });
+/* task04 
+process.argv.forEach(
+    function(element) 
+    {
+        console.log(element);
+    }
+    );*/
+
+/* task05 */
+task05();
+
+function task05()
+{
+    let re = new RegExp("[a-z]:([\\/]?([^*|\\/:\"<>]*))*");
+    if (re.test(process.argv[2]))
+    {
+        let fs = require('fs');
+        let pathToFile = process.argv[2] + "/summary.js";
+        fs.writeFile(pathToFile, "console.log('hello')", function(err) {
+                if(err) 
+                {
+                    throw err;
+                }
+                console.log("The file was created!");
+            });
+        //fs.appendFileSync(pathToFile, ""
+            
+    }
+    else
+    {
+        console.log("Error: not valid path to directory.");
+    }
+    console.log(process.argv[2]);
+}
