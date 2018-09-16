5957dc6bd5c3d11aa8b648059dea3124d1be4f5a
Andrei Chayeuski <andreichayeuski@gmail.com>
2018-09-16 23:35:06 +0300

    we can make dir in other .js file

--- index.js
+++ index.js
@@ -19,20 +19,35 @@ task05();
 
 function task05()
 {
+    const pathToFile = process.argv[2] + "/summary.js";
+    const path = require('path');
     let re = new RegExp("[a-z]:([\\/]?([^*|\\/:\"<>]*))*");
     if (re.test(process.argv[2]))
     {
+
         let fs = require('fs');
-        let pathToFile = process.argv[2] + "/summary.js";
-        fs.writeFile(pathToFile, "console.log('hello')", function(err) {
-                if(err) 
+        fs.writeFile(path.basename(pathToFile), "", function(err) {
+                if (err) 
                 {
                     throw err;
                 }
                 console.log("The file was created!");
             });
-        //fs.appendFileSync(pathToFile, ""
-            
+        
+            fs.appendFileSync(pathToFile, "console.log('" + path.dirname(pathToFile) + "');\n");
+            fs.readdir(path.dirname(pathToFile), function(err, listOfContents)
+            {
+                if (err) 
+                {
+                    console.log(err);
+                }
+                listOfContents.forEach(element => 
+                {
+                    fs.appendFileSync(pathToFile, "console.log('" + path.relative(path.dirname(pathToFile), element) + "');\n");
+                });
+            })
+            fs.appendFileSync(pathToFile, "console.log('" + path.dirname(pathToFile) + "');\n");
+            console.log("All is done.");
     }
     else
     {
